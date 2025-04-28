3919bb6e0b90dadefacbd8153003832196a8a358
DarkhanKanatuly <darhankanatuli1@gmail.com>
2025-04-29 04:35:30 +0500

    Added Create Master link and improved styles

--- AtelierHub/obj/Debug/net8.0/AtelierHub.AssemblyInfo.cs
+++ AtelierHub/obj/Debug/net8.0/AtelierHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc085068e018113428adac8de192ef05f8018a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b43b62a21a7949613f5f43fb4dd508e62c65e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
