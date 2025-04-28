60c17d5792db898a6e3a0544f4703edc051527f9
DarkhanKanatuly <darhankanatuli1@gmail.com>
2025-04-28 20:03:37 +0500

    Fix Dockerfile build context and csproj path

--- AtelierHub/obj/Debug/net8.0/AtelierHub.AssemblyInfo.cs
+++ AtelierHub/obj/Debug/net8.0/AtelierHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc9d5daaf523293d25c7cb22742ceffd3339733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc085068e018113428adac8de192ef05f8018a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtelierHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
