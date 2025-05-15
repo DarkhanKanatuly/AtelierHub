ecbb1dfd6833f4030c0465e641def7458a89c6cf
DarkhanKanatuly <darhankanatuli1@gmail.com>
2025-05-15 11:24:12 +0500

    Fix Swagger, Create button, add photo upload, enhance UI/UX

--- Program.cs
+++ Program.cs
@@ -55,11 +55,8 @@ builder.Services.AddDataProtection()
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AtelierHub API v1"));
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AtelierHub API v1"));
 
 if (!app.Environment.IsDevelopment())
 {
